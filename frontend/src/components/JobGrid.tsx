30c110d80f268f2e43c3f4266a1e286ca26a9e40
Nimrod891 <nimrod891@gmail.com>
2025-11-28 22:04:53 +0200

    fix: grid for wide screens should have 5 cards, laptop should have 4

--- frontend/src/components/JobGrid.tsx
+++ frontend/src/components/JobGrid.tsx
@@ -34,7 +34,7 @@ const JobGrid = ({ refreshKey = 0 }: JobGridProps) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
         padding="20px"
         spacing={10}
       >
